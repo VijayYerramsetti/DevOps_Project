1d342c472266c8fa9bebe11105ea555909d85b26
VijayYerramsetti <yerramsettivijay120@gmail.com>
2024-10-31 12:21:48 -0500

    Terraform IAC

--- EKS-Terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "backend88364" # Replace with your actual S3 bucket name
-    key    = "terraform.tfstate"
-    region = "ap-south-1" # ap-south-1
-  }
-}
